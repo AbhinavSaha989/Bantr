c764cb475651d258e1cfb356ecf0b9fdfd92ffbe
Abhinav Saha <abhinavsaha04@gmail.com>
2025-02-06 00:39:09 +0530

    logout fix

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,7 +4,12 @@ import User from "../models/user.model.js";
 
 const logoutUser = async (req, res) => {
   try {
-    res.clearCookie("jwt");
+    res.clearCookie("jwt","",{
+      httpOnly: true,
+      sameSite: "none",
+      secure: process.env.NODE_ENV === "production",
+      expires: new Date(0),
+    });
     res.status(200).json({
       message: "User logged out successfully",
     });
